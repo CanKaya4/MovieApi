4c2e2f10a1ac18d71bc5c54e3425cf6be8407e56
90534 <90534@ALI>
2025-02-09 21:50:38 +0300

    Context class oluşturuldu

---
+++ Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Persistence.Context
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ALI\\SQLEXPRESS;Database=myDataBase;Trusted_Connection=True;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+
+    }
+}
